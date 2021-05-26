37add1772786483e8d4146a8d04084e22b3afb48
Glitch (glitch-hello-node) <none>
2021-05-25 21:41:10 -0300

    cors added

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 var faunadb = require('faunadb'),
   q = faunadb.query;
 const express = require('express')
-
+const cors = require('cors')
 require('dotenv').config();
+
+const app = express()
+app.use(cors())
+const port = process.env.PORT || 3000
 
 const client = new faunadb.Client({
   secret: process.env.FAUNA_KEY
 });
 
-const app = express()
-const port = process.env.PORT || 3000
-
-
 app.get('/', async (req, res) => {
-
   const response = await client.query(
     q.Get(q.Ref(q.Collection('comercio'), '299596695499440642'))
   )
-
   res.send(response)
 })
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
