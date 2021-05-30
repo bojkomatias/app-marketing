5375954c71253b1ac7749085a45e36a7e9ba6d25
Glitch (glitch-hello-node) <none>
2021-05-30 18:55:20 -0300

    Lista de prods en oferta, con Hateoas

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var faunadb = require('faunadb'),
   q = faunadb.query;
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors');
+const { Lambda } = require('faunadb');
+
 require('dotenv').config();
 
 const app = express()
@@ -12,10 +14,23 @@ const client = new faunadb.Client({
   secret: process.env.FAUNA_KEY
 });
 
-app.get('/', async (req, res) => {
+app.get('/sales/:discount', async (req, res) => {
+  // const response = req.params.discount
   const response = await client.query(
-    q.Get(q.Ref(q.Collection('comercio'), '299596695499440642'))
+    q.Map(
+      q.Paginate(
+        q.Match(q.Index('get_discount_prods'), req.params.discount)
+      ),
+      Lambda('prod', q.Get(q.Var('prod')))
+    )
   )
+  response.data.forEach(element => {
+
+    if (element.data.stock > 0) {
+      element['link'] = `https://strong-traveling-panther.glitch.me/product/${element.ref.id}`
+    }
+    console.log(element)
+  });
   res.send(response)
 })
 
